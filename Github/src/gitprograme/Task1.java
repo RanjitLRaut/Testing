b6cc41f1050de90176c355cb829eab44dc282379
vijaykumargirivishwnath <vijaykumar.v.giri@gmail.com>
2022-02-13 22:34:59 +0530

    by Nilesh

--- Github/src/gitprograme/Task1.java
+++ Github/src/gitprograme/Task1.java
@@ -3,6 +3,11 @@ package gitprograme;
 public class Task1 
 {
 	
+	public void m1()
+	{
+		System.out.println("new code");
+	}
+	
 public static void main(String[] args) 
 {
 	System.out.println("Hello main");
